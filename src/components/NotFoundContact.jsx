f9a6cc9f69a4caa47a96e547336e2ee819948893
Sunny <kumarsunny63588@gmail.com>
2023-10-29 17:56:31 +0530

    second

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -1,8 +1,8 @@
 
 const NotFoundContact = () => {
   return (
-    <div className="flex gap-6 mt-10">
-        <img src="/contact.png" alt=""/>
+    <div className="flex justify-center gap-6 mt-10">
+        <img src="../contact.png" alt=""/>
         <h3 className="text-white text-2xl font-semibold mt-3" >No Contact Found !</h3>
     </div>
   )
