b55f5ef1fcee0dc3795f5db39a3552d56a463fd7
Sunny <kumarsunny63588@gmail.com>
2023-10-30 20:07:50 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
   return (
     <div className=" my-4 h-[60px] bg-white rounded-lg flex items-center justify-center gap-2 text-xl font-medium">  
-        <img src="/firebase.svg/" />
+        <img src="/firebase.svg" />
         <h1>Firebase Contact App</h1>
     </div>
   )
--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -2,7 +2,7 @@
 const NotFoundContact = () => {
   return (
     <div className="flex justify-center gap-6 mt-10">
-        <img src="/contact.png/"/>
+        <img src="/contact.png"/>
         <h3 className="text-white text-2xl font-semibold mt-3" >No Contact Found !</h3>
     </div>
   )
